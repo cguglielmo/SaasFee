d4db40aa7a9ab18339fcb16cdfca8559589d24f1
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2014-05-26 19:09:15 +0200

    Added images and initial script

---
+++ source/script.js
@@ -0,0 +1,24 @@
+function newReddit() {
+    /*jshint multistr: true */
+    var redditHtml = '\
+      <div class="rating">50</div>\
+        <h1>Text-Only-Beitrag</h1>\
+      <p class="text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>\
+      <div class="actionBar">\
+        <span class="newComment">Kommentieren</span>\
+        <span class="share">Teilen</span>\
+      <span class="more">Mehr</span>\
+      </div>\
+      <div class="details">Submitted<br>3 hours ago<br>by bruno asdf asf asdfsdfasdf<br>to /r/subreddit</div>';
+
+    var reddits = document.getElementById('reddits');
+
+    var hr = document.createElement('hr');
+    hr.setAttribute('class', 'hr');
+    reddits.appendChild(hr);
+
+    var reddit = document.createElement('div');
+    reddit.setAttribute('class', 'reddit');
+    reddit.innerHTML = redditHtml;
+    reddits.appendChild(reddit);
+}
