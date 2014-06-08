fdd6f23bd91c7c3a08a20cdc3cce8390b0c0abe6
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2014-06-08 18:03:33 +0200

    added youtube and image embedding

--- source/script.js
+++ source/script.js
@@ -29,11 +29,11 @@ function createNewReddit() {
     var content;
     if (linkField.value) {
         var link = linkField.value;
-        if (link.indexOf('http://') !== 0 || link.indexOf('https://') !== 0) {
-            link = 'http://' + link;
-        }
-        title = '<a href="' + link + '">' + title + '</a>';
-        content = '<video></video>';
+        var url = parseLink(link);
+
+        title = '<a href="' + url.fullUrl + '">' + title + '</a>';
+
+        content = createContent(url);
     }
     content += '<p class="text">' + textField.value.replace(new RegExp('\n', 'g'), '<br>') + '</p>';
 
@@ -50,6 +50,97 @@ function createNewReddit() {
     redditElement.setAttribute('class', 'reddit');
     redditElement.innerHTML = reddit;
     reddits.insertBefore(redditElement, reddits.firstChild);
+}
+
+function parseLink(link) {
+    var url = {}, index, length;
+    if(link.indexOf('//') === 0) {
+        url.scheme = '//';
+        url.path = link.substr(2);
+    }
+    else if(link.indexOf('http://') === 0 || link.indexOf('https://') === 0) {
+        index = link.indexOf(':');
+        url.scheme = link.substr(0, index);
+        url.path = link.substr(index + 3);
+    }
+    else {
+        url.scheme = 'http';
+        url.path = link;
+        link = url.scheme +'://' + url.path;
+    }
+
+    var indexSlash = url.path.indexOf('/');
+    if(indexSlash > 0) {
+        url.domain = url.path.substr(0, indexSlash);
+        url.path= url.path.substr(indexSlash+1);
+    }
+    else {
+        url.domain = url.path;
+        url.path = '';
+    }
+
+    url.extension = extractExtension(link);
+    url.fullUrl = link;
+    return url;
+}
+
+function extractExtension(link) {
+    var extensionIndex;
+
+    extensionIndex = link.lastIndexOf('.');
+    if (extensionIndex < 0) {
+        return;
+    }
+
+    return link.substr(extensionIndex + 1);
+}
+
+function createContent(url) {
+    var linkType = computeLinkType(url);
+    if (linkType === 'image') {
+        var image = document.createElement('img');
+        image.setAttribute('src', url.fullUrl);
+        return image.outerHTML;
+    } else if (linkType === 'youtube') {
+        var src = url.fullUrl;
+        if (src.indexOf('embed') < 0) {
+            var path = url.path;
+            if(path.indexOf('watch?v=') >= 0) {
+                path = path.replace('watch?v=', '');
+            }
+            src = '//www.youtube.com/embed/' + path;
+        }
+
+        var iframe = document.createElement('iframe');
+        iframe.setAttribute('src', src);
+        iframe.setAttribute('class', 'youtube');
+        return iframe.outerHTML;
+    }
+}
+
+function computeLinkType(link) {
+    if (link.domain.indexOf('youtu.be') === 0 || link.domain.indexOf('www.youtube.com') === 0) {
+        return 'youtube';
+    }
+
+    var types = ['png', 'jpg', 'jpeg', 'gif', 'bmp', 'tif'];
+    if (checkExtension(link.extension, types)) {
+        return 'image';
+    }
+
+    //extensions = ['mp4', 'm4v', 'mp4v', 'ogv', 'ogg', 'webm'];
+    //if (checkExtension(link.extension, extensions)) {
+    //    return 'video';
+    //}
+}
+
+function checkExtension(extension, types) {
+    for (var i = 0; i < types.length; i++) {
+        if (extension === types[i]) {
+            return true;
+        }
+    }
+    return false;
 }
 
 function showComments() {
