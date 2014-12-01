c6243dfa683b7f128b3d915be24d6711132eca58
Bruno Koeferli <Bruno.Koeferli@bsiag.com>
2014-12-01 18:04:11 +0100

    changed init sample data entries to match "new" requirements

--- server/data/initSampleData.js
+++ server/data/initSampleData.js
@@ -10,7 +10,7 @@ db.users.insert({
 db.users.insert({
     "name": "Köferli",
     "prename": "Bruno",
-    "email": "bruno.köferli@hsr.ch",
+    "email": "bruno.koeferli@hsr.ch",
     "password": "bko1234"
 });
 
@@ -18,7 +18,7 @@ db.reddits.insert({
     "title": "Text-Only-Beitrag",
     "link": "",
     "text": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-    "date": "14.09.2014",
+    "date": "2014-09-14T16:58:47.326Z",
     "author": "cgu",
     "rating": 1234,
     "commentCount": 2
@@ -26,14 +26,14 @@ db.reddits.insert({
     db.comments.insert({
         "redditId": reddit._id,
         "text": "Blab blalrledledle",
-        "date": "14.09.2014",
+        "date": "2014-09-14T17:58:47.326Z",
         "author": "bko",
         "rating": 0
     });
     db.comments.insert({
         "redditId": reddit._id,
         "text": "Super Bild!",
-        "date": "15.09.2014",
+        "date": "2014-09-14T17:59:47.326Z",
         "author": "cgu",
         "rating": 15
     });
@@ -43,7 +43,7 @@ db.reddits.insert({
     "title": "Link zu Video",
     "link": "//www.youtube.com/embed/C-y70ZOSzE0",
     "text": "",
-    "date": "15.09.2014",
+    "date": "2014-09-15T16:58:47.326Z",
     "author": "bko",
     "rating": 1230,
     "commentCount": 2
@@ -51,14 +51,14 @@ db.reddits.insert({
     db.comments.insert({
         "redditId": reddit._id,
         "text": "Blab blalrledledle",
-        "date": "14.09.2014",
+        "date": "2014-09-16T09:58:47.326Z",
         "author": "bko",
         "rating": 0
     });
     db.comments.insert({
         "redditId": reddit._id,
         "text": "Super Bild!",
-        "date": "15.09.2014",
+        "date": "2014-09-24T16:58:47.326Z",
         "author": "cgu",
         "rating": 15
     });
@@ -68,7 +68,7 @@ db.reddits.insert({
     "title": "Link zu Bild",
     "link": "http://www.ticketcorner.ch/obj/media/CH-eventim/galery/222x222/s/sfv-tickets.gif",
     "text": "",
-    "date": "20.10.2014",
+    "date": "2014-10-14T16:58:47.326Z",
     "author": "cgu",
     "rating": 13433,
     "commentCount": 1
@@ -76,7 +76,7 @@ db.reddits.insert({
     db.comments.insert({
         "redditId": reddit._id,
         "text": "Blab blalrledledle",
-        "date": "14.09.2014",
+        "date": "2014-11-14T16:58:47.326Z",
         "author": "bko",
         "rating": 0
     });
